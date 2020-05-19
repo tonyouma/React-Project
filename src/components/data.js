f4fe5f87878c67344bcb46f01cec5d52b163db5c
TonyOuma <kyrietony@gmail.com>
2020-05-19 13:26:23 +0300

    include(data file):
    to contain the information of the partners

---
+++ src/components/data.js
@@ -0,0 +1,37 @@
+export const partnersItem = [
+  {
+    caption:
+      "How many people will be asking questions? From experience I can tell you there’s nothing like walking into a room and seeing nine people on the other side of the table.Second, what will you be asked.",
+    img: "../graphics/icons8-bbc-150.png",
+  },
+  {
+    caption:
+      "How many people will be asking questions? From experience I can tell you there’s nothing like walking into a room and seeing nine people on the other side of the table.Second, what will you be asked.",
+    img: "../graphics/facebook-logo.png",
+  },
+  {
+    caption:
+      "How many people will be asking questions? From experience I can tell you there’s nothing like walking into a room and seeing nine people on the other side of the table.Second, what will you be asked.",
+    img: "../graphics/pulse-live.png",
+  },
+  {
+    caption:
+      "How many people will be asking questions? From experience I can tell you there’s nothing like walking into a room and seeing nine people on the other side of the table.Second, what will you be asked.",
+    img: "../graphics/Unilever.png",
+  },
+  {
+    caption:
+      "How many people will be asking questions? From experience I can tell you there’s nothing like walking into a room and seeing nine people on the other side of the table.Second, what will you be asked.",
+    img: "../graphics/microsoft1.png",
+  },
+  {
+    caption:
+      "How many people will be asking questions? From experience I can tell you there’s nothing like walking into a room and seeing nine people on the other side of the table.Second, what will you be asked.",
+    img: "../graphics/icons8-bbc-150.png",
+  },
+  {
+    caption:
+      "How many people will be asking questions? From experience I can tell you there’s nothing like walking into a room and seeing nine people on the other side of the table.Second, what will you be asked.",
+    img: "../graphics/Unilever.png",
+  },
+];
